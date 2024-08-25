61d87750ad23402768e5f3942ad4778db3f44cba
Takaya Ueno <takayaueno0712@gmail.com>
2024-08-25 14:45:34 +1000

    Plane Challenge Unit 1

---
+++ PlayerControl/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(0, 5, -15);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - plane.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Position behind plane based on current position
+        Vector3 currentPosition = plane.transform.position + plane.transform.TransformDirection(offset);
+        transform.position = currentPosition;
+        transform.LookAt(plane.transform.position);
+    }
+}
