eedb98fe3bff5131ffe487ae71ceacf1ac75c07a
Takaya Ueno <takayaueno0712@gmail.com>
2024-08-27 18:07:25 +1000

    Added basic gameplay challenge from unit 2

---
+++ BasicGameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    public GameObject dogPrefab;
+    public float timer = 2.0f;
+    public float countDown = 0.0f;
+
+    // Update is called once per frame
+    void Update() {
+        countDown += Time.deltaTime;
+        if (countDown >= timer) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                spawnDog();
+                countDown = 0;
+            }
+        }
+    }
+    void spawnDog() {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
